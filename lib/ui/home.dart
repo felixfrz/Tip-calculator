36c1dc9432c3e8012a8632a61b830ac8b20722ba
unknown <oladoyinayodeji@gmail.com>
2021-08-19 22:03:55 +0100

    updated the ui and some functionalities

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -34,7 +34,7 @@ class _TipCalculatorState extends State<TipCalculator> {
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Text('Total per person'),
+                    Text('Total Per Person'),
                     Text('\$123'),
                   ],
                 ),
@@ -46,7 +46,9 @@ class _TipCalculatorState extends State<TipCalculator> {
               decoration: BoxDecoration(
                 color: Colors.transparent,
                 border: Border.all(
-                    color: Colors.blueGrey.shade100, style: BorderStyle.solid),
+                  color: Colors.blueGrey.shade100,
+                  style: BorderStyle.solid,
+                ),
                 borderRadius: BorderRadius.circular(12.0),
               ),
               child: Column(
@@ -66,6 +68,87 @@ class _TipCalculatorState extends State<TipCalculator> {
                         _billAmount = 0.0;
                       }
                     },
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Split',
+                        style: TextStyle(color: Colors.grey.shade700),
+                      ),
+                      Row(
+                        children: [
+                          InkWell(
+                            onTap: () {
+                              setState(
+                                () {
+                                  if (_personcounter > 1) {
+                                    _personcounter--;
+                                  } else {
+                                    // do nothing
+                                  }
+                                },
+                              );
+                            },
+                            child: Container(
+                              width: 40.0,
+                              height: 40.0,
+                              margin: EdgeInsets.all(10.0),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(7.0),
+                                color: _purple.withOpacity(0.1),
+                              ),
+                              child: Center(
+                                child: Text(
+                                  '-',
+                                  style: TextStyle(
+                                    color: _purple,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 17.0,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          Text(
+                            '$_personcounter',
+                            style: TextStyle(color: _purple, fontSize: 17.0),
+                          ),
+                          InkWell(
+                            onTap: () {
+                              setState(() {
+                                _personcounter++;
+                              });
+                            },
+                            child: Container(
+                              width: 40.0,
+                              height: 40.0,
+                              margin: EdgeInsets.all(10.0),
+                              decoration: BoxDecoration(
+                                  color: _purple.withOpacity(0.1),
+                                  borderRadius: BorderRadius.circular(7.0)),
+                              child: Center(
+                                child: Text(
+                                  '+',
+                                  style: TextStyle(
+                                    color: _purple,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 17.0,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('Tip'),
+                      Text('\$$_tipPercentage'),
+                    ],
                   ),
                 ],
               ),
